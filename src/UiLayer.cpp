c613de97d0318cfa6cbe4fb0f44571ca2d7c92b0
Tom Carden <tom@tom-carden.co.uk>
2011-03-14 17:41:42 -0700

    fixed debug button bug for now

--- src/UiLayer.cpp
+++ src/UiLayer.cpp
@@ -78,7 +78,7 @@ bool UiLayer::touchesBegan( TouchEvent event )
 		mIsPanelTabTouched = false;
 	}
 		
-	return mIsPanelTabTouched;
+	return false; //mIsPanelTabTouched;
 }
 
 bool UiLayer::touchesMoved( TouchEvent event )
@@ -90,7 +90,7 @@ bool UiLayer::touchesMoved( TouchEvent event )
 		setPanelPos( mTouchPos.y, false );
 	}
 
-	return mIsPanelTabTouched;
+	return false;// mIsPanelTabTouched;
 }
 
 bool UiLayer::touchesEnded( TouchEvent event )
@@ -113,7 +113,7 @@ bool UiLayer::touchesEnded( TouchEvent event )
 		}
 	}
 	
-	return mIsPanelTabTouched;
+	return false; //mIsPanelTabTouched;
 }
 
 
