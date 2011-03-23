e9761f37ce9894a973e9877a48852a469ef9aa95
Tom Carden <tom@tom-carden.co.uk>
2011-03-22 23:10:52 -0700

    forget the simplebutton idea for now :)

--- include/SimpleButton.h
+++
@@ -1,7 +0,0 @@
-//
-//  SimpleButton.h
-//  Kepler
-//
-//  Created by Tom Carden on 3/22/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
--- src/SimpleButton.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SimpleButton.cpp
-//  Kepler
-//
-//  Created by Tom Carden on 3/22/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include "SimpleButton.h"
