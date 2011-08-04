c3af30b9c96385b4509b47ad1ebc06786004283c
Tom Carden <tom@tom-carden.co.uk>
2011-08-04 12:14:28 -0700

    stubbing out threaded texture loading, again

--- src/TextureLoader.cpp
+++ src/TextureLoader.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "TextureLoader.h"
+#include "cinder/app/AppCocoaTouch.h" // for loadResource
+#include "cinder/Thread.h"
+#include "cinder/ImageIo.h"
+
+using namespace ci;
+using namespace ci::app;
+
+void TextureLoader::start()
+{
+    mState = RUNNING;
+    std::thread loaderThread( &TextureLoader::loadSurfaces, this );  
+}
+
+void TextureLoader::loadSurfaces()
+{
+    for (int i = 0; i < mRequests.size(); i++) {
+        mRequests[i].mSurface = loadImage( loadResource( mRequests[i].mFileName ) );
+        mProgress = i;
+    }
+    mState = COMPLETE;
+}
--- src/TextureLoader.h
+++ src/TextureLoader.h
@@ -5,3 +5,44 @@
 //  Created by Tom Carden on 7/25/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include "cinder/Surface.h"
+
+class TextureLoader {
+public:
+
+    enum State { WAITING, RUNNING, COMPLETE };
+    
+    struct Request {
+        int mId;
+        std::string mFileName;
+        ci::Surface mSurface;
+        
+        Request( int reqId, std::string fileName ): mId(reqId), mFileName(fileName) {}
+    };        
+    
+    TextureLoader( std::vector<Request> requests ): mRequests(requests), mState(WAITING), mProgress(0) {} 
+    
+    int getProgress() { return mProgress; }
+    
+    int getCount() { return mRequests.size(); }
+    
+    // check for COMPLETE before making textures
+    State getState() { return mState; }
+    
+    // begin the thread
+    void start();
+        
+private:
+    
+    void loadSurfaces();
+        
+    std::vector<Request> mRequests;
+    State mState;
+    int mProgress;
+
+};
