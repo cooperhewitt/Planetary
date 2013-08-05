1aa249d3b0230d468f6729a3c6f44d312dc7c69b
straup <straup@gmail.com>
2013-08-05 10:49:40 -0700

    include all the blocks

--- bin/src2pdf.sh
+++ bin/src2pdf.sh
@@ -18,8 +18,17 @@ then
     PDF="${ROOT}/planetary.pdf"
 fi
 
+if [ -f $PDF ]
+then
+    echo "${PDF} already exists, removing in 5 seconds..."
+    sleep 5
+    rm ${PDF}
+fi
+
 echo "writing source code to ${PDF}"
 
-${ENSCRIPT} --toc -p - --line-numbers -B --file-align=2 --word-wrap -O -f 'Courier8' ${ROOT}/src/* | ${PSTOPDF} -i -o ${PDF}
+# Note that we are excluding blocks/CinderFlurry/lib/FlurryLib/libFlurry.a
+
+${ENSCRIPT} -v --toc --margins=30:30:50:50 --non-printable-format=octal -p - --line-numbers --header='6814.1.2013|$N|$%/$=' --file-align=2 --word-wrap -O -F 'Courier6' -f 'Courier8' ${ROOT}/src/* ${ROOT}/blocks/BloomScene/include/* ${ROOT}/blocks/BloomScene/src/* ${ROOT}/blocks/BloomTasks/src/* ${ROOT}/blocks/CinderFlurry/include/* ${ROOT}/blocks/CinderFlurry/lib/FlurryLib/*.h ${ROOT}/blocks/CinderFlurry/src/* ${ROOT}/blocks/CinderGestures/include/* ${ROOT}/blocks/CinderIPod/include/* ${ROOT}/blocks/CinderIPod/src/* ${ROOT}/blocks/CinderOrientation/include/* ${ROOT}/blocks/CinderOrientation/src/*  | ${PSTOPDF} -i -o ${PDF}
 
 exit
