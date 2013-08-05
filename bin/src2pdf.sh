251c0eb5e5b51929539a95b602f215e05b8e01cd
straup <straup@gmail.com>
2013-08-05 11:06:01 -0700

    OCRA; landscape mode and legal size to account for linewraps and page runoffs

--- bin/src2pdf.sh
+++ bin/src2pdf.sh
@@ -2,6 +2,8 @@
 
 # See also:
 # http://linux.die.net/man/1/enscript
+# http://ansuz.sooke.bc.ca/page/fonts#ocra
+# http://sourceforge.jp/projects/tsukurimashou/downloads/56948/ocr-0.2.zip/
 
 PYTHON=`which python`
 ENSCRIPT=`which enscript`
@@ -29,6 +31,6 @@ echo "writing source code to ${PDF}"
 
 # Note that we are excluding blocks/CinderFlurry/lib/FlurryLib/libFlurry.a
 
-${ENSCRIPT} -v --toc --margins=30:30:50:50 --non-printable-format=octal -p - --line-numbers --header='6814.1.2013|$N|$%/$=' --file-align=2 --word-wrap -O -F 'Courier6' -f 'Courier8' ${ROOT}/src/* ${ROOT}/blocks/BloomScene/include/* ${ROOT}/blocks/BloomScene/src/* ${ROOT}/blocks/BloomTasks/src/* ${ROOT}/blocks/CinderFlurry/include/* ${ROOT}/blocks/CinderFlurry/lib/FlurryLib/*.h ${ROOT}/blocks/CinderFlurry/src/* ${ROOT}/blocks/CinderGestures/include/* ${ROOT}/blocks/CinderIPod/include/* ${ROOT}/blocks/CinderIPod/src/* ${ROOT}/blocks/CinderOrientation/include/* ${ROOT}/blocks/CinderOrientation/src/*  | ${PSTOPDF} -i -o ${PDF}
+${ENSCRIPT} -v -r --media=Legal --toc --margins=30:30:50:50 --non-printable-format=octal -p - --line-numbers --header='6814.1.2013|$N|$%/$=' --word-wrap -O -F 'OCRA6' -f 'OCRA6' ${ROOT}/src/* ${ROOT}/blocks/BloomScene/include/* ${ROOT}/blocks/BloomScene/src/* ${ROOT}/blocks/BloomTasks/src/* ${ROOT}/blocks/CinderFlurry/include/* ${ROOT}/blocks/CinderFlurry/lib/FlurryLib/*.h ${ROOT}/blocks/CinderFlurry/src/* ${ROOT}/blocks/CinderGestures/include/* ${ROOT}/blocks/CinderIPod/include/* ${ROOT}/blocks/CinderIPod/src/* ${ROOT}/blocks/CinderOrientation/include/* ${ROOT}/blocks/CinderOrientation/src/*  | ${PSTOPDF} -i -o ${PDF}
 
 exit
